186e8094bedf3551c1592905dbb25a05233a4678
constanzavicencio <constanzavicencioc@gmail.com>
2024-06-21 19:51:06 -0400

    third commit

--- T4/cliente/frontend.py
+++ T4/cliente/frontend.py
@@ -1,10 +1,137 @@
 from backend import ClientBackend
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, 
                              QPushButton, QLabel, QLineEdit, QComboBox, QGridLayout, 
-                             QHBoxLayout, QScrollArea, QFormLayout)
-from PyQt6.QtCore import Qt, QTimer
+                             QHBoxLayout, QScrollArea, QFormLayout, QFrame)
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QThread
 from PyQt6.QtGui import QPixmap
 import sys
+import numpy as np
+import json
+import os
+import socket
+
+from parametros import *
+
+# Constante para el tamaño de las celdas
+CELL_SIZE = 20
+
+# Board: clase que representa internamente el estado del juego
+class Board:
+    def __init__(self, filename):
+        self.filename = filename
+        self.load_board()
+        self.i = 0
+        self.j = 0
+
+    def load_board(self):
+        with open(self.filename, 'r') as file:
+            puzzle_data = file.readlines()
+            self.puzzle_up = puzzle_data[0].strip()
+            self.puzzle_left = puzzle_data[1].strip()
+
+        self.p_up_list = [list(map(int, x.split(','))) if x != '-' else [0] for x in self.puzzle_up.split(';')]
+        self.p_left_list = [list(map(int, x.split(','))) if x != '-' else [0] for x in self.puzzle_left.split(';')]
+
+        self.width = len(self.p_up_list)
+        self.height = len(self.p_left_list)
+        self.grid = np.ones((self.width, self.height))
+
+    def __str__(self):
+        return f"current position: {self.i}, {self.j}\n{self.grid}"
+    
+    def getCurrentPosition(self):
+        return (self.i, self.j)
+
+    def getHeight(self):
+        return self.height
+
+    def getWidth(self):
+        return self.width
+
+    def moveLeft(self):
+        self.j = max(0, self.j-1)
+
+    def moveRight(self):
+        self.j = min(self.width-1, self.j+1)
+
+    def moveUp(self):
+        self.i = max(self.i-1, 0)
+
+    def moveDown(self):
+        self.i = min(self.height-1, self.i+1)
+
+    def eat(self):
+        self.grid[self.i][self.j] = 0
+
+    def poop(self):
+        self.grid[self.i][self.j] = 2
+
+    def isEmpty(self):
+        return self.grid[self.i][self.j] == 0
+
+# GameController: conecta el Board con la pantalla
+class GameController:
+    def __init__(self, Board, ClientFrontend):
+        self.Board = Board
+        self.ClientFrontend = ClientFrontend
+
+        self.game_board = self.ClientFrontend.game_board
+
+        self.player = QLabel(self.ClientFrontend)
+        self.player.setFixedSize(CELL_SIZE, CELL_SIZE)
+        self.player.setStyleSheet("border: 1px solid black;")
+        self.player.setPixmap(QPixmap("assets/sprites/pepa/down_0.png"))
+        self.player.setScaledContents(True)
+
+        self.updatePlayer(self.player)
+
+    def updatePlayer(self, player):
+        i = self.Board.getCurrentPosition()[0]
+        j = self.Board.getCurrentPosition()[1]
+        player.setParent(None)
+        self.game_board.addWidget(player, i, j)
+
+    def updateBoard(self):
+        for i in range(self.Board.getHeight()):
+            for j in range(self.Board.getWidth()):
+                item = self.game_board.itemAtPosition(i, j)
+                if item is not None:
+                    widget = item.widget()
+                    if self.Board.grid[i][j] == 0:
+                        widget.setPixmap(QPixmap())
+                    elif self.Board.grid[i][j] == 1:
+                        widget.setPixmap(QPixmap("assets/sprites/lechuga.png"))
+                    elif self.Board.grid[i][j] == 2:
+                        widget.setPixmap(QPixmap("assets/sprites/poop.png"))
+
+    def moveLeft(self):
+        self.Board.moveLeft()
+        self.updatePlayer(self.player)
+
+    def moveRight(self):
+        self.Board.moveRight()
+        self.updatePlayer(self.player)
+
+    def moveUp(self):
+        self.Board.moveUp()
+        self.updatePlayer(self.player)
+
+    def moveDown(self):
+        self.Board.moveDown()
+        self.updatePlayer(self.player)
+
+    def eat_or_poop(self):
+        i, j = self.Board.getCurrentPosition()
+        if self.Board.isEmpty():
+            self.Board.poop()
+            QTimer.singleShot(TIEMPO_TRANSICION * 1000, lambda: self.convertPoopToLettuce(i, j))
+        else:
+            self.Board.eat()
+        self.updateBoard()
+
+    def convertPoopToLettuce(self, i, j):
+        self.Board.grid[i][j] = 1
+        self.updateBoard()
 
 class ClientFrontend(QMainWindow):
     def __init__(self, config_path):
@@ -24,18 +151,25 @@ class ClientFrontend(QMainWindow):
 
         self.start_screen()
 
-    def start_screen(self):
-        self.clear_layout()
+    def load_board(self, filename):
+        self.board = Board(filename)
+        self.connectBoardToGridLayout(self.board, self.row_indicator, self.col_indicator, self.game_board)
+        self.controller = GameController(self.board, self)
 
-        # Logo
+    def start_screen(self):
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QGridLayout()
+        self.central_widget.setLayout(self.layout)
+
         self.logo = QLabel(self)
         pixmap = QPixmap("assets/sprites/logo.png")
-        scaled_pixmap = pixmap.scaled(200, 100, Qt.AspectRatioMode.KeepAspectRatio)  # Adjust size
+        scaled_pixmap = pixmap.scaled(200, 100, Qt.AspectRatioMode.KeepAspectRatio)
         self.logo.setPixmap(scaled_pixmap)
         self.logo.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.logo, 0, 0, 1, 3)
 
-        # Salón de la fama
         self.fame_label = QLabel("Salón de la Fama", self)
         self.fame_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.fame_label.setStyleSheet("font-size: 18px; margin: 10px;")
@@ -47,17 +181,15 @@ class ClientFrontend(QMainWindow):
         self.fame_layout = QVBoxLayout()
         self.fame_widget.setLayout(self.fame_layout)
 
-        # Cargar los puntajes (aquí se agregan manualmente para el ejemplo)
-        scores = ["Jugador1: 1000", "Jugador2: 900", "Jugador3: 850"]
-        for score in scores:
-            label = QLabel(score, self)
+        scores = self.load_scores()
+        for name, score in scores:
+            label = QLabel(f"{name}: {score}", self)
             self.fame_layout.addWidget(label)
 
         self.scroll_area.setWidget(self.fame_widget)
-        self.scroll_area.setStyleSheet("background-color: #f0f0f0; margin: 10px;")
+        self.scroll_area.setStyleSheet("background-color: #000000; margin: 10px;")
         self.layout.addWidget(self.scroll_area, 2, 0, 3, 1)
 
-        # Formulario para ingresar nombre de usuario y seleccionar puzzle
         form_layout = QFormLayout()
 
         self.username_input = QLineEdit(self)
@@ -65,12 +197,11 @@ class ClientFrontend(QMainWindow):
         form_layout.addRow("Nombre de usuario:", self.username_input)
 
         self.puzzle_dropdown = QComboBox(self)
-        self.puzzle_dropdown.addItems(["Puzzle 1", "Puzzle 2", "Puzzle 3"])  # Agrega los nombres de los puzzles disponibles
+        self.puzzle_dropdown.addItems(self.load_puzzles())
         form_layout.addRow("Seleccione un puzzle:", self.puzzle_dropdown)
 
         self.layout.addLayout(form_layout, 2, 1, 1, 2)
 
-        # Botones
         self.start_button = QPushButton("Comenzar partida", self)
         self.start_button.clicked.connect(self.start_game)
         self.start_button.setStyleSheet("background-color: #4CAF50; color: white; font-weight: bold;")
@@ -81,62 +212,107 @@ class ClientFrontend(QMainWindow):
         self.exit_button.setStyleSheet("background-color: #f44336; color: white; font-weight: bold;")
         self.layout.addWidget(self.exit_button, 4, 2, 1, 1)
 
+    def load_scores(self):
+        scores = []
+        if os.path.exists("puntaje.txt"):
+            with open("puntaje.txt", "r") as file:
+                for line in file:
+                    try:
+                        name, score = line.strip().split(': ')
+                        scores.append((name, float(score)))
+                    except ValueError:
+                        print(f"Línea con formato incorrecto: {line.strip()}")
+        return sorted(scores, key=lambda x: x[1], reverse=True)
+
+    def load_puzzles(self):
+        puzzles = []
+        base_puzzles_path = "assets/base_puzzles"
+        for filename in os.listdir(base_puzzles_path):
+            if filename.endswith(".txt"):
+                puzzles.append(filename)
+        return puzzles
+
     def start_game(self):
         self.username = self.username_input.text()
+        if not self.validate_username(self.username):
+            self.show_message("Nombre de usuario no válido. Debe ser alfanumérico, contener al menos una mayúscula y un número.")
+            return
+
         self.puzzle = self.puzzle_dropdown.currentText()
-
-        # Conectar al servidor
         self.backend.connect(8000)
-
         self.game_screen()
 
+    def validate_username(self, username):
+        return (username.isalnum() and 
+                any(char.isupper() for char in username) and 
+                any(char.isdigit() for char in username))
+
     def game_screen(self):
-        self.clear_layout()
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        outermost_grid = QGridLayout(central_widget)
 
-        self.timer_label = QLabel("Tiempo: 60", self)
-        self.layout.addWidget(self.timer_label, 0, 0, 1, 3)
+        self.gamewindow = QGridLayout()
+        blank = QGridLayout()
+        self.col_indicator = QGridLayout()
+        self.row_indicator = QGridLayout()
 
-        self.grid = QGridLayout()
-        self.layout.addLayout(self.grid, 1, 0, 1, 3)
-
-        self.create_game_board()
-
-        self.button_layout = QHBoxLayout()
-
-        self.pause_button = QPushButton("Pausar", self)
-        self.pause_button.clicked.connect(self.pause_game)
-        self.button_layout.addWidget(self.pause_button)
-
-        self.validate_button = QPushButton("Validar Respuesta", self)
-        self.validate_button.clicked.connect(self.validate_solution)
-        self.button_layout.addWidget(self.validate_button)
-
-        self.exit_button = QPushButton("Salir", self)
-        self.exit_button.clicked.connect(self.close)
-        self.button_layout.addWidget(self.exit_button)
-
-        self.layout.addLayout(self.button_layout, 2, 0, 1, 3)
+        self.game_board = QGridLayout()
+        self.gamewindow.addLayout(blank, 0, 0)
+        self.gamewindow.addLayout(self.col_indicator, 0, 1)
+        self.gamewindow.addLayout(self.row_indicator, 1, 0)
+        self.gamewindow.addLayout(self.game_board, 1, 1)
+        
+        button_board = QGridLayout()
+        time_left_layout = QVBoxLayout()
+        countdown_label = QLabel("Tiempo: ", self)
+        self.timer_label = QLabel("00:00", self)
+        time_left_layout.addWidget(countdown_label)
+        time_left_layout.addWidget(self.timer_label)
 
         self.timer = QTimer(self)
+        self.time_remaining = TIEMPO_JUEGO
         self.timer.timeout.connect(self.update_timer)
-        self.time_remaining = 60
         self.timer.start(1000)
 
-    def create_game_board(self):
-        self.board_size = 5  # Example size, should be dynamic based on puzzle
-        self.cells = []
+        check_button = QPushButton("Comprobar", self)
+        check_button.clicked.connect(self.validate_solution)
+        self.pause_button = QPushButton("Pausar", self)
+        self.pause_button.clicked.connect(self.pause_game)
+        exit_button = QPushButton("Salir", self)
+        exit_button.clicked.connect(self.close)
+        button_board.addLayout(time_left_layout, 0, 0)
+        button_board.addWidget(check_button, 2, 0)
+        button_board.addWidget(self.pause_button, 3, 0)
+        button_board.addWidget(exit_button, 4, 0)
 
-        for row in range(self.board_size):
-            row_cells = []
-            for col in range(self.board_size):
+        outermost_grid.addLayout(self.gamewindow, 0, 0)
+        outermost_grid.addLayout(button_board, 0, 1)
+
+        self.load_board(f"assets/base_puzzles/{self.puzzle}")
+
+    def connectBoardToGridLayout(self, board, row_indicator, col_indicator, game_board):
+        frame = QFrame()
+        frame.setFrameShape(QFrame.Shape.Box)
+        frame.setLineWidth(1)
+        n = board.getHeight()
+        m = board.getWidth()
+        for i in range(n):
+            row_text = str(board.p_left_list[i])
+            row_label = QLabel(row_text, self)
+            row_indicator.addWidget(row_label, i, 0)
+        for i in range(m):
+            col_text = str(board.p_up_list[i])
+            col_label = QLabel(col_text, self)
+            col_indicator.addWidget(col_label, 0, i)
+        for i in range(n):
+            for j in range(m):
                 cell = QLabel(self)
-                cell.setFixedSize(40, 40)
+                cell.setFixedSize(CELL_SIZE, CELL_SIZE)
                 cell.setStyleSheet("border: 1px solid black;")
                 cell.setPixmap(QPixmap("assets/sprites/lechuga.png"))
                 cell.setScaledContents(True)
-                self.grid.addWidget(cell, row, col)
-                row_cells.append(cell)
-            self.cells.append(row_cells)
+                game_board.addWidget(cell, i, j)
 
     def pause_game(self):
         if self.timer.isActive():
@@ -147,24 +323,43 @@ class ClientFrontend(QMainWindow):
             self.pause_button.setText("Pausar")
 
     def validate_solution(self):
-        solution = "mock_solution"  # Aquí iría la solución real del puzzle
+        solution = "\n".join(["".join(map(str, row)) for row in self.board.grid])
         response = self.backend.send_message(solution)
         if response == "OK":
             self.show_message("Solución correcta")
+            self.save_score()
         else:
             self.show_message("Solución incorrecta")
 
+    def save_score(self):
+        score = (self.time_remaining * self.board.getHeight() * self.board.getWidth() * CONSTANTE) / (300 - self.time_remaining)
+        score_entry = f"{self.username}: {round(score, 2)}"
+        with open("puntaje.txt", "a") as file:
+            file.write(f"{score_entry}\n")
+        self.start_screen()
+
     def update_timer(self):
         self.time_remaining -= 1
-        self.timer_label.setText(f"Tiempo: {self.time_remaining}")
+        minutes, seconds = divmod(self.time_remaining, 60)
+        self.timer_label.setText(f"{minutes:02}:{seconds:02}")
         if self.time_remaining <= 0:
             self.show_message("Tiempo agotado")
             self.start_screen()
 
     def show_message(self, message):
-        self.clear_layout()
-        self.layout.addWidget(QLabel(message, self))
-        self.layout.addWidget(QPushButton("Volver a Inicio", self, clicked=self.start_screen))
+        # Crear un nuevo layout para mostrar el mensaje
+        message_layout = QVBoxLayout()
+        message_layout.addWidget(QLabel(message, self))
+        back_button = QPushButton("Volver a Inicio", self)
+        back_button.clicked.connect(self.start_screen)
+        message_layout.addWidget(back_button)
+        
+        # Crear un widget para contener el layout del mensaje
+        message_widget = QWidget()
+        message_widget.setLayout(message_layout)
+        
+        # Establecer el widget del mensaje como el central
+        self.setCentralWidget(message_widget)
 
     def clear_layout(self):
         while self.layout.count():
@@ -174,15 +369,15 @@ class ClientFrontend(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_W:
-            print("Mover hacia arriba")
+            self.controller.moveUp()
         elif event.key() == Qt.Key.Key_A:
-            print("Mover hacia la izquierda")
+            self.controller.moveLeft()
         elif event.key() == Qt.Key.Key_S:
-            print("Mover hacia abajo")
+            self.controller.moveDown()
         elif event.key() == Qt.Key.Key_D:
-            print("Mover hacia la derecha")
+            self.controller.moveRight()
         elif event.key() == Qt.Key.Key_G:
-            print("Marcar/Desmarcar casilla")
+            self.controller.eat_or_poop()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ T4/cliente/parametros.py
@@ -0,0 +1,16 @@
+# 3. Mecánicas de Juego
+## Modo de Juego
+TIEMPO_JUEGO = 120 # (segundos)
+## Puntaje
+CONSTANTE = 10
+
+# 4. Entidades
+## Pepa
+TIEMPO_TRANSICION = 3
+## Sandía
+TIEMPO_APARICION = 30
+TIEMPO_ADICIONAL = 15
+TIEMPO_DURACION = 10
+
+# 6. Cheatcodes
+PUNTAJE_INF = 10
